87d2a24c525408502e2adbe8042f580b891b2b3f
mahdieh-amiri1 <99467399+mahdieh-amiri1@users.noreply.github.com>
2023-10-09 10:37:10 +0330

    Update QuadraticFunding.sol

--- contracts/QuadraticFunding.sol
+++ contracts/QuadraticFunding.sol
@@ -10,23 +10,21 @@ contract QuadraticFunding is Ownable {
 
     DeLT internal _DeLT; // Reference to the DeLT token contract
 
-    // Mapping for project ID to its count of contributions
-    mapping (uint256 => uint256) public contributionsCount;
+    struct Project {
+        uint256 contributionsCount;
+        uint256 contributionsAmount;
+        address owner;
+        uint256 matchingFund;
+    }
 
-    // Mapping for project ID to the total contribution amount
-    mapping (uint256 => uint256) public contributionsAmount;
-
-    // Mapping for project ID to its owner address
-    mapping (uint256 => address) public projectOwners;
-
-    // Mapping for project ID to its matching fund
-    mapping (uint256 => uint256) public projectMatchingFunds;
+    mapping (uint256 => Project) public projects; // Mapping for project ID to project details
 
     uint256 public totalProjectsCount; // Total count of added projects
-    bool public withdrawEnabled; // Flag to enable withdrawal of matching funds
-    uint256 public matchingPool; // Total amount available for matching
-    uint256 public pendingWithdraws; // Count of pending withdrawals
+    bool public withdrawEnabled;       // Flag to enable withdrawal of matching funds
+    uint256 public matchingPool;       // Total amount available for matching
+    uint256 public pendingWithdraws;   // Count of pending withdrawals
 
+    // Events
     event MatchingFundWithdrawn(uint256 indexed projectId, address indexed creator);
     event MatchingPoolIncreased(uint256 indexed addedAmount, uint256 indexed currentAmount);
     event FundsWithdrawn(uint256 indexed withdrawnAmount, uint256 currentAmount);
@@ -46,17 +44,23 @@ contract QuadraticFunding is Ownable {
         emit MatchingPoolIncreased(amountToPool, matchingPool);
     }
 
-    /// @dev Add a project to the tracking system.
+    /// @dev Add a project to the matching projects.
     /// @param projectId The ID of the project to be added.
     /// @param contributionAmount The total contribution amount for the project.
     function addToMatchingProjects(uint256 projectId, uint256 contributionAmount) internal {
-        projectOwners[projectId] = msg.sender;
-        contributionsAmount[projectId] = contributionAmount;
+        projects[projectId] = Project({
+            contributionsCount: 0,
+            contributionsAmount: contributionAmount,
+            owner: msg.sender,
+            matchingFund: 0
+        });
         totalProjectsCount++;
     }
 
+    /// @dev Add a contribution to a matching project.
+    /// @param projectId The ID of the project to add a contribution.
     function addToContributions(uint256 projectId) internal {
-        contributionsCount[projectId]++;
+        projects[projectId].contributionsCount++;
     }
 
     /// @dev Update the matching funds for each project.
@@ -64,13 +68,18 @@ contract QuadraticFunding is Ownable {
     function updateMatchingFunds() internal onlyOwner returns (bool) {
         uint256 projectsCount = totalProjectsCount;
         uint256[] memory powerTwoOfSumOfSquareRoots = new uint256[](projectsCount);
+        uint256[] memory contributionsCount = new uint256[](projectsCount);
         uint256 totalPowerTwoOfSumOfSquareRoots;
         uint256 totalFund = matchingPool;
+
+        for (uint256 i = 0; i < projectsCount; i++){
+            contributionsCount[i] = projects[i].contributionsCount;
+        }
 
         for (uint256 i = 0; i < projectsCount; i++) {
             uint256 contributions = contributionsCount[i];
             if (contributions > 0) {
-                uint256 amount = contributionsAmount[i];
+                uint256 amount = projects[i].contributionsAmount;
                 uint256 sumOfSquareRoots = contributions * sqrt(amount);
                 powerTwoOfSumOfSquareRoots[i] = sumOfSquareRoots * sumOfSquareRoots;
                 totalPowerTwoOfSumOfSquareRoots += powerTwoOfSumOfSquareRoots[i];
@@ -80,9 +89,8 @@ contract QuadraticFunding is Ownable {
         require(totalPowerTwoOfSumOfSquareRoots > 0, "No contributors found");
 
         for (uint256 i = 0; i < projectsCount; i++) {
-            uint256 contributions = contributionsCount[i];
-            if (contributions > 0) {
-                projectMatchingFunds[i] += powerTwoOfSumOfSquareRoots[i] * totalFund / totalPowerTwoOfSumOfSquareRoots;
+            if (contributionsCount[i] > 0) {
+                projects[i].matchingFund += powerTwoOfSumOfSquareRoots[i] * totalFund / totalPowerTwoOfSumOfSquareRoots;
                 pendingWithdraws++;
             }
         }
@@ -102,11 +110,11 @@ contract QuadraticFunding is Ownable {
     /// @param projectId The ID of the project for which funds are being withdrawn.
     function withdrawMatchingFund(uint256 projectId) public {
         require(withdrawEnabled, "Withdraw is not enabled");
-        uint256 matchingAmount = projectMatchingFunds[projectId];
-        require(projectOwners[projectId] == msg.sender, "Only project owner can withdraw");
+        uint256 matchingAmount = projects[projectId].matchingFund;
+        require(projects[projectId].owner == msg.sender, "Only project owner can withdraw");
         require(matchingAmount > 0, "No matching amount");
-        projectMatchingFunds[projectId] = 0;
-        contributionsCount[projectId] = 0;
+        projects[projectId].matchingFund = 0;
+        projects[projectId].contributionsCount = 0;
         require(_DeLT.transfer(msg.sender, matchingAmount), "Token transfer failed");
         matchingPool -= matchingAmount;
         pendingWithdraws--;
@@ -135,8 +143,8 @@ contract QuadraticFunding is Ownable {
     function withdrawFunds(uint256 amount) external onlyOwner {
         require(amount <= _DeLT.balanceOf(address(this)), "Insufficient balance");
         matchingPool -= amount;
-        bool transferSuccess = _DeLT.transfer(owner(), amount);
-        require(transferSuccess, "Token transfer failed");
+        require(_DeLT.transfer(owner(), amount), "Token transfer failed");
         emit FundsWithdrawn(amount, matchingPool);
     }
+
 }

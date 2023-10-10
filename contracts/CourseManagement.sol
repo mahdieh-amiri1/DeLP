5b20873a1a7fef2281751476f50a7c878fb39ab2
mahdieh-amiri1 <99467399+mahdieh-amiri1@users.noreply.github.com>
2023-10-10 18:12:20 +0330

    Update CourseManagement.sol

--- contracts/CourseManagement.sol
+++ contracts/CourseManagement.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
-// import "./DeLT.sol";
 import "./SoulBoundCertificate.sol";
 import "./QuadraticFunding.sol";
 

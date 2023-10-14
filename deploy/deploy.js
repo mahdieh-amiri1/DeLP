1a856a4162188d7462c904bba9cd56a23b693459
Mahdieh <99467399+mahdieh-amiri1@users.noreply.github.com>
2023-10-14 22:01:47 +0330

    Update deploy.js

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,1 +1,26 @@
+// const { network } = require("hardhat")
+// const { verify } = require("../utils/verify")
 
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+  await deploy("DeLT", {
+    contract: "DeLT",
+    from: deployer,
+    log: true,
+  })
+
+  const delt = await deployments.get("DeLT")
+  const courseManagement = await deploy("CourseManagement", {
+    from: deployer,
+    args: [delt.address],
+    log: true,
+  })
+
+  log("Contracts deployed")
+
+  // if (network.name == "sepolia") {
+  //   await verify(delt.address, [])
+  //   await verify(courseManagement.address, [delt.address])
+  // }
+}
